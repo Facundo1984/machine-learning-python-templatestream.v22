dc9acdd9549d71e9aa19daa1b52d051d06fa84c2
Facundo <137770302+Facundo1984@users.noreply.github.com>
2023-11-29 20:29:26 +0000

    ok

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-def buscar_jugador_similar(nombre, posicion, edad, habilidad):
+def buscar_jugador_similar(nombre, sexo, posicion, edad, habilidad):
     # Aquí podrías tener una lógica más avanzada basada en tus criterios de búsqueda
     # En este ejemplo, simplemente se devuelve un nombre aleatorio
     nombres_similares = ["Messi", "Ronaldo", "Neymar", "Mbappé"]
@@ -21,12 +21,13 @@ def main():
     st.title("Buscador de Jugadores de Fútbol Similares")
     # Formulario para ingresar los criterios de búsqueda
     nombre = st.text_input("Nombre del Jugador:")
+    sexo = st.selectbox("Sexo:", ["Femenino", "Masculino"])
     posicion = st.selectbox("Posición:", ["Delantero", "Centrocampista", "Defensa", "Portero"])
     edad = st.slider("Edad:", min_value=16, max_value=40, value=25)
     habilidad = st.slider("Habilidad (1-100):", min_value=1, max_value=100, value=50)
     # Botón para realizar la búsqueda
     if st.button("Buscar Jugador Similar"):
-        jugadores_similares = buscar_jugador_similar(nombre, posicion, edad, habilidad)
+        jugadores_similares = buscar_jugador_similar(nombre, sexo, posicion, edad, habilidad)
         if jugadores_similares:
             st.success(f"Jugadores con habilidades similares : {nombre}: {', '.join(jugadores_similares)}")
         else:
